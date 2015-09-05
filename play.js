d71f1a992c421bed7b104f6c5c48ba4f343723ef
cponeill <caseyoneill78@hotmail.com>
2015-09-05 19:51:36 +0000

    adding code

--- play.js
+++ play.js
@@ -1,1 +1,17 @@
+var array = [1, 2, 3, 4, 5];
 
+function forEach(array, action) {
+    for (var i = 0; i < array.length; i++) {
+        action(array[i]);
+    }
+}
+
+function addArray(array, numbers) {
+    var count = 1;
+    forEach(array, function(number) {
+        count += number
+    });
+    return count;
+}
+
+console.log(addArray(array, console.log));
