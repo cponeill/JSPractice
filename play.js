0824dcdf48f867f8cdd55daa895d4c77c63d17c6
Casey O'Neill <caseyoneill78@hotmail.com>
2015-09-10 11:31:29 -0700

    Update play.js

--- play.js
+++ play.js
@@ -172,13 +172,7 @@ console.log(every([NaN, NaN, 4], isNaN));
 console.log(some([NaN, 3, 4], isNaN));
 console.log(some([2, 3, 4], isNaN));
 
-var myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-function linearSearch(A, n) {
-  for (var i = 0; i < A.length; i++) {
-    if (A[i] == n) {
-      return i;
-    }
-  }
+// add binary search here
+function binarySearch(A, n) {
+     // wire code up here.
 }
-
-console.log(linearSearch(myArray, 6));
